3c22ae3ee7ee16068effb9efe2350a5825af7ca9
Syed-Kashan-Adil <syed.kashan.adil@gmail.com>
2022-01-10 00:54:31 +0500

    nextjs boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,77 @@
+/* eslint-disable global-require */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    borderColor: (theme) => ({
+      ...theme('colors'),
+      primary: '#00C4CC',
+    }),
+    boxShadow: {
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      DEFAULT:
+        '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      '3xl': 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none',
+    },
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    extend: {
+      height: {
+        26: '104px',
+      },
+      margin: {
+        18: '4.5rem',
+      },
+      colors: {
+        gradientPrimary: '#DB9CE1',
+        primary: '#00C4CC',
+        primaryHover: '#4ca4a8',
+        primaryDark: '#058f97',
+        secondary: '#A9C9FF',
+        primaryGray: '#EEEEEE',
+        secondaryGrey: '#707070',
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
+        blue: {
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#90cdf4',
+          400: '#63b3ed',
+          500: '#4299e1',
+          600: '#3182ce',
+          700: '#2b6cb0',
+          800: '#2c5282',
+          900: '#2a4365',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/forms')],
+};
