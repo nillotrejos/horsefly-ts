1785db595daca95f470b95ebec4fc2acce0702dd
Syed-Kashan-Adil <syed.kashan.adil@gmail.com>
2022-01-10 01:11:55 +0500

    feat: configure husky eslint and prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["prettier --write", "eslint --fix", "eslint"],
-  "**/*.ts?(x)": () => "npm run build-types",
-  "*.json": ["prettier --write"],
+  '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix', 'eslint'],
+  '**/*.ts?(x)': () => 'npm run build-types',
+  '*.json': ['prettier --write']
 };
