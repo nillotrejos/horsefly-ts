7cda9746bccfb705f416d910f3fc4d8abaa87be0
Syed-Kashan-Adil <syed.kashan.adil@gmail.com>
2022-01-30 02:16:39 +0500

    setup redux toolkit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next';
 import HomePage from './HomePage/home';
+import { store } from '../store/store';
+import { Provider } from 'react-redux';
 const Home: NextPage = () => {
   return (
     <div>
-      <HomePage />
+      <Provider store={store}>
+        <HomePage />
+      </Provider>
+      ,
     </div>
   );
 };
